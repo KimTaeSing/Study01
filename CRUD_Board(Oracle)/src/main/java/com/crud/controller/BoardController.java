e99340a8a87a5adf79f8eed04af158ebca11cbaa
seong <seong@192.168.219.108>
2023-03-21 16:23:55 +0900

    어노테이션 사용처 설명 추가 및 RedirectAttributes 사용의도

--- CRUD_Board(Oracle)/src/main/java/com/crud/controller/BoardController.java
+++ CRUD_Board(Oracle)/src/main/java/com/crud/controller/BoardController.java
@@ -12,12 +12,15 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import com.crud.model.BoardVO;
 import com.crud.service.BoardService;
 
+// 해당 클래스를 컨트롤러로 사용하겠다는 어노테이션 
 @Controller
+// 어떤 컨트롤러에 어떤 메소드가 처리할지를 매핑하는 어노테이션 !
 @RequestMapping("/board/*")
 public class BoardController {
 	
 	private static final Logger log = LoggerFactory.getLogger(BoardController.class);
 	
+	// 필요한 의존객체 타입에 해당하는 빈을 찾아 주입하는 어노테이션 ! 
 	@Autowired
 	private BoardService bservice;
 	
@@ -37,6 +40,7 @@ public class BoardController {
 	public String boardEnrollPOST(BoardVO board, RedirectAttributes rttr) {
 		log.info("데이터 테스트 : "+ board);
 		bservice.enroll(board);
+		// 사용자가 등록을 완료했다는 피드백을 알려주기 위해 RedirectAttributes 메소드를 시용 하였음 ! 
 		 rttr.addFlashAttribute("result", "success");
 		
 		return "redirect:/board/list";
